ccb776555b7644856de14f659a59be07443387ee
mrawcliffe <mar44@aber.ac.uk>
2016-03-22 16:51:33 +0000

    clean input for ratings

--- gouda/modules/ratings/main.py
+++ gouda/modules/ratings/main.py
@@ -9,11 +9,16 @@ def main(*args, **kwargs):
     writer = kwargs.pop('writer')
     line = kwargs.pop('line')
     offset = 0
-    if line[0][:-1] == kwargs.get('name', 'Gouda'):
-        offset = 1
-    if line[offset] == "rate":
-        writer("%d/8" % random.randint(0, 8))
-    elif line[offset][0] == 'r' and line[offset][1:].isdigit():
-        base = int(line[offset][1:])
-        rating = random.randint(0, base)
-        writer("%d/%d" % (rating, base))
+    if len(line[0]) > 1:
+        if line[0][:-1] == kwargs.get('name', 'Gouda'):
+            offset = 1
+        if line[offset] == "rate":
+            writer("%d/8" % random.randint(0, 8))
+        elif line[offset][0] == 'r' and line[offset][1:].isdigit():
+            try:
+                base = int(line[offset][1:])
+                rating = random.randint(0, base)
+                writer("%d/%d" % (rating, base))
+            except ValueError:
+                # naughty input
+                pass
