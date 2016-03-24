f4e479d82e7616f82d08425db3fbbe8da43f4948
Matthew Rawcliffe <mhrawcliffe@gmail.com>
2016-03-24 22:01:09 +0000

    qtags added and feedback on saving quote

--- gouda/modules/quote/main.py
+++ gouda/modules/quote/main.py
@@ -20,9 +20,10 @@ def main(*args, **kwargs):
     # since this module has special syntax we use a main func
     writer = kwargs.pop('writer')
     line = kwargs.pop('line')
-    if line[0].startswith("qsave") and len(line) > 2:
+    if line[0] == "qsave" and len(line) > 2:
         # saving a new quote
         tag = line[1]
+        saved = False
         if line[2].startswith("^") and len(line[2]) > 1:
             last_id = Message.select().order_by(Message.id.desc()).get().id
             try:
@@ -31,6 +32,7 @@ def main(*args, **kwargs):
                     try:
                         message = Message.select().where(Message.id==last_id-num).get()
                         Quote.create(tag=tag, user=message.name, message=message.message)
+                        saved = True
                     except Message.DoesNotExist:
                         # just in case
                         pass
@@ -40,7 +42,10 @@ def main(*args, **kwargs):
         else:
             message = ' '.join(line[2:])
             Quote.create(tag=tag, user=kwargs.pop('nick', '~'), message=message)
-    elif line[0].startswith("qload") and len(line) > 1:
+            saved = True
+        if saved:
+            writer("Saved your quote, pal")
+    elif line[0] == "qload" and len(line) > 1:
         # retrieving a quote
         tag = line[1]
         try:
@@ -49,7 +54,7 @@ def main(*args, **kwargs):
         except:
             # probably no quotes in this tag
             print("Could not load quote with tag %s" % tag)
-    elif line[0].startswith("qfind") and len(line) > 1:
+    elif line[0] == "qfind" and len(line) > 1:
         # search records
         try:
             quote = Quote.select().where(Quote.message.contains(' '.join(line[1:]))).order_by(fn.Random()).get()
@@ -57,3 +62,9 @@ def main(*args, **kwargs):
         except:
             # probably no quotes in this tag
             print("Could not find quote with pattern %s" % ' '.join(line[1:]))
+    elif line[0] == "qtags":
+        try:
+            tags = Quote.select(Quote.tag).distinct()
+            writer('Tags: %s.' % ', '.join(t.tag for t in tags))
+        except:
+            print("Cannot load tags")
